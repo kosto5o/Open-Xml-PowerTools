ab9a027a6a3c88355e190790775b8b385a3ce1b6
lowell <lowello@gmail.com>
2019-01-15 17:25:00 -0700

    abstract data sources and field formats for DocumentAssembler

---
+++ OpenDocx/OpenDocx/DocumentAssembler/IDataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDocx
+{
+    interface IDataContext : IMetadataParser
+    {
+        string EvaluateText(string selector, bool optional);
+        IDataContext[] EvaluateList(string selector, bool optional);
+    }
+
+    public class EvaluationException : Exception
+    {
+        public EvaluationException() { }
+        public EvaluationException(string message) : base(message) { }
+        public EvaluationException(string message, Exception inner) : base(message, inner) { }
+    }
+
+}
---
+++ OpenDocx/OpenDocx/DocumentAssembler/IMetadataParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace OpenDocx
+{
+    interface IMetadataParser
+    {
+        string DelimiterOpen { get; }
+        string DelimiterClose { get; }
+        string EmbedOpen { get; }
+        string EmbedClose { get; }
+        XElement TransformContentToMetadata(string content);
+    }
+
+    public class MetadataParseException : Exception
+    {
+        public MetadataParseException() { }
+        public MetadataParseException(string message) : base(message) { }
+        public MetadataParseException(string message, Exception inner) : base(message, inner) { }
+    }
+
+}
